8e534d053c9a9cf9118bd5eb403b0ee18b0d2be3
PheonixArch <arslanali.ghaffar1234@gmail.com>
2024-01-23 17:49:41 +0500

    somechangesoccur

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -16,9 +16,9 @@ const textVariants = {
   },
   scrollButton: {
     opacity: 0,
-    y: 10,
+    y: 30,
     transition: {
-      duration: 2,
+      duration: 1,
       repeat: Infinity,
     },
   },
@@ -47,19 +47,20 @@ const Hero = () => {
           initial="initial"
           animate="animate"
         >
-          <motion.h2 variants={textVariants}>HARVEY TYLER</motion.h2>
+          <motion.h2 variants={textVariants}>HAFIZ ARSLAN ALI</motion.h2>
           <motion.h1 variants={textVariants}>
-            Web developer and UI designer
+            Web Developer and Designer
           </motion.h1>
-          <motion.div variants={textVariants} className="buttons">
+          {/* <motion.div variants={textVariants} className="buttons">
             <motion.button variants={textVariants}>
               See the Latest Works
             </motion.button>
+           
             <motion.button variants={textVariants}>Contact Me</motion.button>
-          </motion.div>
+          </motion.div> */}
           <motion.img
             variants={textVariants}
-            animate="scrollButton"
+            animate="scrollButton" 
             src="/scroll.png"
             alt=""
           />
@@ -71,11 +72,11 @@ const Hero = () => {
         initial="initial"
         animate="animate"
       >
-        Writer Content Creator Influencer
+        Web Developer Graphic Designer Problem Solver Gaming Rookie
       </motion.div>
-      <div className="imageContainer">
+      {/* <div className="imageContainer">
         <img src="/hero.png" alt="" />
-      </div>
+      </div> */}
     </div>
   );
 };
